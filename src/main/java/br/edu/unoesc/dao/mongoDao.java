9ce202c1e499d811b7a2e9b9d86641ba4d4686f0
Rafael Argenta <rafiqueud@gmail.com>
2016-03-19 17:57:07 -0300

    Fixado

--- src/main/java/br/edu/unoesc/dao/mongoDao.java
+++ src/main/java/br/edu/unoesc/dao/mongoDao.java
@@ -11,55 +11,48 @@ import com.mongodb.MongoClient;
 import br.edu.unoesc.modelo.Funcionario;
 
 public class mongoDao {
-
 	private static mongoDao mg;
-
 	private Jongo jongo;
-	
-	public static mongoDao getDAO(){
-		if (mg == null){
+
+	public static mongoDao getDAO() {
+		if (mg == null) {
 			mg = new mongoDao();
 		}
 		return mg;
 	}
-	
-	public mongoDao(){
+
+	public mongoDao() {
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
 		jongo = new Jongo(mongoClient.getDB("meclegal"));
 	}
-	
-	
-	
-	public ArrayList<Funcionario> listaFuncionarios(){
-		MongoCursor<Funcionario> cursor = jongo.getCollection("br.edu.unoesc.modelo.Funcionario").find().as(Funcionario.class);
-		
+
+	public ArrayList<Funcionario> listaFuncionarios() {
+		MongoCursor<Funcionario> cursor = jongo.getCollection("br.edu.unoesc.modelo.Funcionario").find()
+				.as(Funcionario.class);
+
 		ArrayList<Funcionario> func = new ArrayList<Funcionario>();
-		
-		cursor.forEach(f->{
+
+		cursor.forEach(f -> {
 			func.add(f);
 		});
-		
-return func;
+
+		return func;
 	}
-	
-	
-	
-	
-	public ArrayList<?> listaTodos(Class<?> classe){
+
+	public ArrayList<?> listaTodos(Class<?> classe) {
 		MongoCollection collection = jongo.getCollection(classe.getName());
 		MongoCursor<?> cursor = collection.find().as(classe);
 		ArrayList array = new ArrayList();
-		cursor.forEach(eta->{
+		cursor.forEach(eta -> {
 			array.add(eta);
 		});
 		return array;
 	}
-	
-	public void inserir(Object objeto){
+
+	public void inserir(Object objeto) {
 		MongoCollection collection = jongo.getCollection(objeto.getClass().getName());
 		System.out.println(objeto.getClass().getName());
 		collection.insert(objeto);
 	}
-	
 
 }

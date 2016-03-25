72fd7b464abfbae865acd2076cb4d82f4c08597b
Tiago <Tiago@192.168.25.8>
2016-03-25 16:19:13 -0300

    Adicinado interface para poder utilizar as telas de busca em todas as telas que se utilizem daquela classe específica.

---
+++ src/main/java/br/edu/unoesc/preencheDados/PreencheDados.java
@@ -0,0 +1,9 @@
+package br.edu.unoesc.preencheDados;
+
+import br.edu.unoesc.modelo.MinhaEntidade;
+
+public interface PreencheDados {
+	
+	public void preencherCampos(MinhaEntidade entidade);
+	
+}

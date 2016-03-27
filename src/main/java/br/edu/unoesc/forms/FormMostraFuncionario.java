e14721a768b86e4ed5943cc193c22fdc38664364
Tiago <Tiago@192.168.25.8>
2016-03-27 18:12:07 -0300

    Adicionado tela para mostrar funcionários cadastrados.

---
+++ src/main/java/br/edu/unoesc/forms/FormMostraFuncionario.java
@@ -0,0 +1,145 @@
+package br.edu.unoesc.forms;
+
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import br.edu.unoesc.dao.MongoDao;
+import br.edu.unoesc.modelo.Funcionario;
+import br.edu.unoesc.preencheDados.PreencheDados;
+
+public class FormMostraFuncionario extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel jpMostraFuncionario;
+	private Vector<String> dados;
+	private JTable jttListaFuncionario;
+	private DefaultTableModel dtmListaFuncionario;
+	private JScrollPane jspListaFuncionario;
+	private JButton jbSelecionar;
+	private JButton jbCancelar;
+	private PreencheDados preencheDados;
+	private Funcionario funcionario;
+	private List<Funcionario> listaFuncionarios;
+	private String descricao;
+	private Integer cracha;
+	
+	public void componentesFormMostraFuncionario() {
+		dados = new Vector<String>();
+		dados.add("Crachá");
+		dados.add("Descrição");
+		dados.add("Valor");
+		
+		dtmListaFuncionario = new DefaultTableModel();
+		dtmListaFuncionario.setColumnIdentifiers(dados);
+		
+		jttListaFuncionario = new JTable();
+		jttListaFuncionario.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jttListaFuncionario.setModel(dtmListaFuncionario);
+		jttListaFuncionario.getColumnModel().getColumn(0).setResizable(false);
+		jttListaFuncionario.getColumnModel().getColumn(0).setPreferredWidth(50);
+		jttListaFuncionario.getColumnModel().getColumn(1).setResizable(false);
+		jttListaFuncionario.getColumnModel().getColumn(1).setPreferredWidth(300);
+		jttListaFuncionario.getColumnModel().getColumn(2).setResizable(false);
+		jttListaFuncionario.getColumnModel().getColumn(2).setPreferredWidth(100);
+		jpMostraFuncionario.setLayout(null);
+		
+		jspListaFuncionario = new JScrollPane(jttListaFuncionario);
+		jspListaFuncionario.setBounds(10, 11, 450, 300);
+		jpMostraFuncionario.add(jspListaFuncionario);
+		
+		jbSelecionar = new JButton("Selecionar");
+		jbSelecionar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jbSelecionar.setBounds(10, 322, 110, 25);
+		jpMostraFuncionario.add(jbSelecionar);
+		
+		jbCancelar = new JButton("Cancelar");
+		jbCancelar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		jbCancelar.setBounds(130, 322, 110, 25);
+		jpMostraFuncionario.add(jbCancelar);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void preencheDadosTabelaPorNome() {
+		listaFuncionarios = (List<Funcionario>) MongoDao.getDAO().listaGenerica(Funcionario.class, "nome", descricao);
+		listaFuncionarios.forEach(funcionario->{
+			dtmListaFuncionario.addRow(funcionario.vetorDados());
+		});
+	}
+	
+	public void preencheDadosTabelaPorCracha() {
+		funcionario = new Funcionario();
+		funcionario = (Funcionario) MongoDao.getDAO().buscaGenerica(Funcionario.class, "cracha", cracha);
+		dtmListaFuncionario.addRow(funcionario.vetorDados());
+	}
+	
+	public void validaPesquisa() {
+		if(descricao != null && !descricao.equals("")) {
+			this.preencheDadosTabelaPorNome();
+		} else if(cracha != null && !cracha.equals("")) {
+			this.preencheDadosTabelaPorCracha();
+		}
+	}
+	
+	public void acionarBotaoSelecionar() {
+		jbSelecionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == jbSelecionar) {
+					Integer funcionarioSelecionado = jttListaFuncionario.getSelectedRow();
+					if(funcionarioSelecionado != -1) {
+						funcionario = listaFuncionarios.get(funcionarioSelecionado);
+						preencheDados.preencherCampos(funcionario);
+						dispose();
+					} else {
+						JOptionPane.showMessageDialog(null, "Nenhum funcionário foi selecionado!!!\n"
+								+ "Por gentileza, selecionar um funcionário!!!", "Erro", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		});
+	}
+	
+	public void acionarBotaoCancelar() {
+		jbCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+	}
+	
+	public void inicializarForm() {
+		listaFuncionarios = new ArrayList<>();
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(FormMostraFuncionario.class.getResource("/br/edu/unoesc/imagens/logo.png")));
+		setTitle("Lista de Funcionários");
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 475, 385);
+		jpMostraFuncionario = new JPanel();
+		setContentPane(jpMostraFuncionario);
+		jpMostraFuncionario.setLayout(null);
+		
+		componentesFormMostraFuncionario();
+		validaPesquisa();
+		acionarBotaoSelecionar();
+		acionarBotaoCancelar();
+	}
+
+	public FormMostraFuncionario(PreencheDados dados, String descricao, Integer codigo) {
+		this.preencheDados = dados;
+		this.descricao = descricao;
+		this.cracha = codigo;
+		inicializarForm();
+	}
+}
